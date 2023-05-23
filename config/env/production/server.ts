23540c190e499cb92adcba5b40db19520d4b18bf
James Latten <james.latten@phntm.com>
2023-05-22 22:07:24 -0400

    Fixes

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,8 +1,8 @@
 
 export default ({ env }) => ({
     proxy: true,
-    url: env('APP_URL', 'https://jotlin-hlrxz.ondigitalocean.app'), // Sets the public URL of the application.
-    port: env.int('PORT', 1337),
+    url: env('APP_URL'), // Sets the public URL of the application.
+    port: env.int('PORT'),
     app: { 
       keys: env.array('APP_KEYS')
     },
