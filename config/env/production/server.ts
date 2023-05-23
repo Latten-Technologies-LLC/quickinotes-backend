4c30797e95c5863ea2a99f5e510bfc12b206e692
James Latten <james.latten@phntm.com>
2023-05-22 20:33:16 -0400

    port

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -2,6 +2,7 @@
 export default ({ env }) => ({
     proxy: true,
     url: env('APP_URL', 'https://jotlin-hlrxz.ondigitalocean.app'), // Sets the public URL of the application.
+    port: env.int('PORT', 1337),
     app: { 
       keys: env.array('APP_KEYS')
     },
